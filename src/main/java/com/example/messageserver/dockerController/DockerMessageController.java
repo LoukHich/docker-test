d846c47828149153c2fbadbae8a60044f650df49
LoukiliHicham <86587991+LoukHich@users.noreply.github.com>
2022-12-16 00:38:53 +0100

    Update Docker Message 1

--- src/main/java/com/example/messageserver/dockerController/DockerMessageController.java
+++ src/main/java/com/example/messageserver/dockerController/DockerMessageController.java
@@ -7,6 +7,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class DockerMessageController {
     @GetMapping("/messages")
     public String getMessage() {
-        return "Hello from Docker2!";
+        return "Hello from Docker  ENSAH !";
     }
 }
