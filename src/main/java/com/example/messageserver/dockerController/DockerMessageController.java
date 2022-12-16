6f95b550d7116fa1c12d4f1fb6a515b42073d226
LoukiliHicham <86587991+LoukHich@users.noreply.github.com>
2022-12-16 01:17:52 +0100

    Update Docker Message 1

--- src/main/java/com/example/messageserver/dockerController/DockerMessageController.java
+++ src/main/java/com/example/messageserver/dockerController/DockerMessageController.java
@@ -7,6 +7,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class DockerMessageController {
     @GetMapping("/messages")
     public String getMessage() {
-        return "WAHD SALAM 3LIKOM MN 3ND docker";
+        return "Bonjour Mes amis ";
     }
 }
