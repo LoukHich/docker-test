369a5492658a75a36c2fc55d2e130d057766126f
LoukiliHicham <86587991+LoukHich@users.noreply.github.com>
2022-12-17 11:30:09 +0100

    add jenkins file

--- src/main/java/com/example/messageserver/dockerController/DockerMessageController.java
+++ src/main/java/com/example/messageserver/dockerController/DockerMessageController.java
@@ -7,6 +7,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class DockerMessageController {
     @GetMapping("/messages")
     public String getMessage() {
-        return "Hello from Docker 2";
+        return "Hello from ENSAH";
     }
 }
